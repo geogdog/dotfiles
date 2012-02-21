d0f95d5dccc11a35f5687d998f087a6c9be0d036
Greg Trahair <greg.trahair@gmail.com>
2012-02-21 20:01:42 +0100

    Added new history features.  Closes issue #3.

--- .ps1
+++ .ps1
@@ -37,7 +37,7 @@ RED='\033[01;31m'
 GREEN='\033[01;32m'
 REGULAR=$RED
 SUPER='\033[00;31m'
-PROMPT_COMMAND='RET=$?;'
+PROMPT_COMMAND='RET=$?;history -a; history -n'
 ret_value='$(echo $RET)'
 ret_smiley='$(if [[ $RET = 0 ]]; then echo -en "\[${GREEN}\]:)"; else echo -en "\[${RED}\]:("; fi;)'
 
